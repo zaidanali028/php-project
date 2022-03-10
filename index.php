984a2ce60a1758162a7274c06c3bcfdf8b6f9486
zaidanali028 <zaidanali028@gmail.com>
2022-03-10 10:24:48 +0000

    fix tuts route

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+// routing logic
+
+$request = $_SERVER['REQUEST_URI'];
+$path = "/Project";
+// echo  $path . '/tuts';
+// echo $request;
+switch ($request) {
+    case $path . '/':
+        require __DIR__ . '/views/main/index.php';
+        break;
+    case '':
+        require __DIR__ . '/views/main/index.php';
+        break;
+
+
+    case $path . '/tuts':
+        require __DIR__ .  '/views/main/tuts.php';
+        break;
+
+
+
+    default:
+        http_response_code(404);
+        require __DIR__ . '/views/404.php';
+        break;
+}
