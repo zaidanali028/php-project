6d4cddc2581ad4c86741e19781ac3938654de238
zaidanali028 <zaidanali028@gmail.com>
2022-03-11 18:34:32 +0000

    php spy

--- index.php
+++ index.php
@@ -2,11 +2,13 @@
 
 // routing logic
 
+
+
 $request = $_SERVER['REQUEST_URI'];
 $path = "/php-project";
-// echo  $path . '/tuts';
-// echo $request;
+
 switch ($request) {
+    // index routes
     case  $path.'/':
         require __DIR__ . '/views/main/index.php';
         break;
@@ -19,9 +21,18 @@ switch ($request) {
         require __DIR__ .  '/views/main/tuts.php';
         break;
  
-
-
-    default:
+       
+    //    admin routes
+        case  $path."/admin":
+            require __DIR__ .  '/views/admin/index.php';
+            break;
+        case  $path."/admin/add":
+            // static css assets aint working here 
+                require __DIR__ .  '/views/admin/index.php';
+            break;
+  
+//   anythign else's routes
+            default:
         http_response_code(404);
         require __DIR__ . '/views/404.php';
         break;
---
+++ views/partials/admin/sidebar.php
@@ -0,0 +1,45 @@
+<nav class="sidebar sidebar-offcanvas" id="sidebar">
+        <ul class="nav">
+          <li class="nav-item">
+            <div class="d-flex sidebar-profile">
+              <div class="sidebar-profile-image">
+                <img src="public/admin/images/faces/face29.png" alt="image">
+                <span class="sidebar-status-indicator"></span>
+              </div>
+              <div class="sidebar-profile-name">
+                <p class="sidebar-name">
+                  Kenneth Osborne
+                </p>
+                <p class="sidebar-designation">
+                  Welcome
+                </p>
+              </div>
+            </div>
+            <div class="nav-search">
+              <div class="input-group">
+                <input type="text" class="form-control" placeholder="Type to search..." aria-label="search" aria-describedby="search">
+                <div class="input-group-append">
+                  <span class="input-group-text" id="search">
+                    <i class="typcn typcn-zoom"></i>
+                  </span>
+                </div>
+              </div>
+            </div>
+            <p class="sidebar-menu-title">Dash menu</p>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="admin/add">
+              <i class="mr-2 mdi mdi-book-open-page-variant"></i>
+              <span class="menu-title">Add A New Tutorial</span>
+            </a>
+          </li>
+        </ul>
+        <ul class="sidebar-legend">
+          <li>
+            <p class="sidebar-menu-title">Category</p>
+          </li>
+          <li class="nav-item"><a href="#" class="nav-link">#Tutorials</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">#Questions</a></li>
+          <li class="nav-item"><a href="#" class="nav-link">#Sign Out</a></li>
+        </ul>
+      </nav>
